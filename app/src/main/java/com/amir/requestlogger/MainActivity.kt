4172c4d871f28267dfea72a7148d3afd9993b6e7
Amir Khan <fury.amir93@gmail.com>
2022-09-27 12:46:23 +0530

    * jitpack dependency

--- app/src/main/java/com/amir/requestlogger/MainActivity.kt
+++ app/src/main/java/com/amir/requestlogger/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.amir.requestlogger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.amir.chuck.RequestLogger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
