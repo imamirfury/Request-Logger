f1ddbf7bac8db247993909d76a91cc17bf340878
Amir Khan <fury.amir93@gmail.com>
2022-12-07 10:54:20 +0530

    * made compatible with minSdk 19

--- app/src/main/java/com/amir/requestlogger/MainActivity.kt
+++ app/src/main/java/com/amir/requestlogger/MainActivity.kt
@@ -2,12 +2,10 @@ package com.amir.requestlogger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.amir.chuck.RequestLogger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RequestLogger(this)
     }
 }
